1241d5e9ddc76336483301f7fa37712d71994025
teddygood <ibear6954@gmail.com>
2021-06-15 19:34:34 +0900

    solve problem 15

--- p015.cpp
+++ p015.cpp
@@ -1,2 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+
+// Combination
+int main()
+{
+    ll ans = 1;
+    for (ll i = 1; i <= 20; i++)
+    {
+        ans *= 40 - i + 1;
+        ans /= i;
+    }
+    cout << ans;
+}
