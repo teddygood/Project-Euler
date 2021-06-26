7aa11068918c5433e08dadc71c28e1e94acb3ba9
teddygood <ibear6954@gmail.com>
2021-06-26 20:20:13 +0900

    update p018

--- p018.cpp
+++ p018.cpp
@@ -1,3 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+
+const int N = 16;
+
+int dp[N + 5][N + 5];
+
+int main()
+{
+    for (int i = 1; i <= N; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            int x;
+            cin >> x;
+            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - 1]) + x;
+        }
+    }
+    int mx = 0;
+    for (int i = 1; i <= N; i++)
+    {
+        mx = max(dp[N][i], mx);
+    }
+}
