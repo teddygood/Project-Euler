3cef62d6dbd590891d5ef8bb1f5a982bc3781839
teddygood <ibear6954@gmail.com>
2021-06-27 20:10:13 +0900

    update p019

---
+++ p019.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int mon[12] = {31, 28, 331, 30,31,30,31,31,30,31,30,31};
+
+bool isLeap(int n){
+    return (n%4==0&&n%100) || n%400==0;
+}
+
+int main()
+{
+
+}
