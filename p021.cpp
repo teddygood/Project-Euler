2bd56d4f20cb53bac178b7b5c0ce45e5fe56b023
teddygood <ibear6954@gmail.com>
2021-08-04 22:07:16 +0900

    update problem 21

--- p021.cpp
+++ p021.cpp
@@ -1,2 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int MAXN = 1e4;
+
+int d[MAXN + 1];
+
+int main()
+{
+    int tot = 0;
+    for (int i = 1; i <= MAXN; i++)
+    {
+        for (int j = 2 * i; j <= MAXN; j += i)
+        {
+            d[j] += i;
+        }
+    }
+    for (int i = 1; i <= MAXN; i++)
+    {
+        if (d[i] <= MAXN && i == d[d[i]] && i != d[i])
+            tot += i;
+    }
+    cout << tot << endl;
+}
