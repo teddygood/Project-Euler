de1da19b01bd63e38ab5079c34be93e7ace507a8
teddygood <ibear6954@gmail.com>
2021-08-04 22:07:02 +0900

    update problem 20

--- p020.cpp
+++ p020.cpp
@@ -1,2 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int arr[200];
+
+void times(int n)
+{
+    for (int i = 0; i < 200; i++)
+        arr[i] *= n;
+}
+
+void op()
+{
+    for (int i = 0; i < 199; i++)
+    {
+        arr[i + 1] += arr[i] / 10;
+        arr[i] %= 10;
+    }
+}
+
+int main()
+{
+    int sum = 0;
+    arr[0] = 1;
+
+    for (int i = 2; i <= 100; i++)
+    {
+        times(i);
+        op();
+    }
+    for (int i = 0; i < 200; i++)
+        sum += arr[i];
+    cout << sum;
+}
